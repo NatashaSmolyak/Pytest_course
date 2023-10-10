30e83871df0a9a67c0b1c0af92797d49cd53b844
NatashaSmolyak <smnatali@yandex.ru>
2023-10-10 19:57:31 +0300

    Add test_burger_menu

---
+++ lesson1/test_burger_menu/test_reset_app_state.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+# 2. Проверка работоспособности кнопки "About" в меню
+def test_about():
+    driver.get("https://www.saucedemo.com/")
+
+    url_before = driver.current_url
+
+    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    username_field.send_keys("standard_user")
+
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys("secret_sauce")
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
+
+    burger_menu = driver.find_element(By.ID, "react-burger-menu-btn")
+    burger_menu.click()
+    time.sleep(2)
+
+    reset_app_state = driver.find_element(By.XPATH, "//a[@id='reset_sidebar_link']")
+    reset_app_state.click()
+
+    url_about = driver.current_url
+    assert url_about == 'https://saucelabs.com/'
